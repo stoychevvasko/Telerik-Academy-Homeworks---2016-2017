391fb9c9f0d283ab9d022d7bc16b5c23ff6ba94a
stoychevvasko <stoychev.vassil@gmail.com>
2016-10-27 17:40:28 +0300

    C# I - homework 4 - 05. Numbers Comparer
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# I/homework_4_c_sharp_due_28.10.2016/05. Numbers comparer/NumbersComparer.cs
@@ -0,0 +1,46 @@
+﻿/**
+Number Comparer
+Description
+
+Write a program that gets two numbers from the console and prints the greater of them.
+Input
+
+    On the first two lines you will receive the two numbers, A and B
+
+Output
+
+    On the only line print the larger of the two numbers
+        *Try implementing it without using if-statements
+
+Constraints
+
+    The input will always be valid and in the described format.
+    The numbers A and B will always be valid real number
+    Time limit: 0.1s
+    Memory limit: 16MB
+
+Sample tests
+Input 	Output
+5
+6 	6
+10
+5 	10
+0
+0 	0
+-5
+-2 	-2
+1.5
+1.6 	1.6
+**/
+
+using System;
+
+class NumbersComparer
+{
+    static void Main()
+    {
+        decimal a = decimal.Parse(Console.ReadLine());
+        decimal b = decimal.Parse(Console.ReadLine());
+        Console.WriteLine(a > b ? a : b);
+    }
+}
