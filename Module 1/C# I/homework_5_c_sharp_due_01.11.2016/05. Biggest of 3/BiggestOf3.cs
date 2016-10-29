9cbacb10398dd66f91c3adbcb09976dd7d3f61b7
stoychevvasko <stoychev.vassil@gmail.com>
2016-10-29 14:25:47 +0300

    C# I - homework 5 - 05. Biggest of 3
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# I/homework_5_c_sharp_due_01.11.2016/05. Biggest of 3/BiggestOf3.cs
@@ -0,0 +1,43 @@
+﻿/**
+Biggest of 3
+
+Description
+
+Write a program that finds the biggest of three numbers that are read from the console.
+
+Input
+
+On the first three lines you will receive the three numbers, each on a separate line.
+Output
+
+On the only output line, write the biggest of the three numbers.
+Constraints
+
+The three numbers will always be valid floating-point numbers in the range [-200, 200].
+Time limit: 0.1s
+Memory limit: 16MB
+Sample tests
+
+Input	Output
+4
+4
+4	4
+-0.5
+-10
+0	0
+**/
+
+using System;
+
+class BiggestOf3
+{
+    static void Main()
+    {
+        decimal[] numbers = new decimal[3];
+        numbers[0] = decimal.Parse(Console.ReadLine());
+        numbers[1] = decimal.Parse(Console.ReadLine());
+        numbers[2] = decimal.Parse(Console.ReadLine());
+        Array.Sort(numbers);
+        Console.WriteLine(numbers[2]);
+    }
+}
