02c19ec23831a944267145aa34e0dd77c7da0215
stoychevvasko <stoychev.vassil@gmail.com>
2016-10-29 14:13:58 +0300

    C# I - homework 5 - 04. Multiplication Sign
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# I/homework_5_c_sharp_due_01.11.2016/04. Multiplication sign/MultiplicationSign.cs
@@ -0,0 +1,65 @@
+﻿/**
+Multiplication sign
+
+Description
+
+Write a program that shows the sign (+, - or 0) of the product of three real numbers, without calculating it.
+
+Use a sequence of if operators.
+Input
+
+On the first three lines, you will receive the three numbers, each on a separate line
+Output
+
+Output a single line - the sign of the product of the three numbers
+Constraints
+
+The input will always consist of valid floating-point numbers
+Time limit: 0.1s
+Memory limit: 16MB
+Sample tests
+
+Input	Output
+2
+5
+2	+
+2
+5
+-2	-
+-0.5
+0
+-2	0
+**/
+
+using System;
+
+class MultiplicationSign
+{
+    static void Main()
+    {
+        decimal num1 = decimal.Parse(Console.ReadLine());
+        decimal num2 = decimal.Parse(Console.ReadLine());
+        decimal num3 = decimal.Parse(Console.ReadLine());
+
+        if (num1 == 0 || num2 == 0 || num3 == 0)
+        {
+            Console.WriteLine("0");
+        }
+        else if (num1 < 0 && (((num2 < 0) && (num3 < 0)) || ((num2 > 0) && (num3 > 0))))
+        {
+            Console.WriteLine("-");
+        }
+        else if (num1 < 0 && (((num2 < 0) && (num3 > 0)) || ((num2 > 0) && (num3 < 0))))
+        {
+            Console.WriteLine("+");
+        }
+        else if (num1 > 0 && (((num2 < 0) && (num3 < 0)) || ((num2 > 0) && (num3 > 0))))
+        {
+            Console.WriteLine("+");
+        }
+        else if (num1 > 0 && (((num2 < 0) && (num3 > 0)) || ((num2 > 0) && (num3 < 0))))
+        {
+            Console.WriteLine("-");
+        }
+    }
+}
