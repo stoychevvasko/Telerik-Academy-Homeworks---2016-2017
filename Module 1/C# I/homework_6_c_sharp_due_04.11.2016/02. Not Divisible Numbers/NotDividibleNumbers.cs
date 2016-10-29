1e39b996d776c6015e53c3ede1e9e02e1d5848d3
stoychevvasko <stoychev.vassil@gmail.com>
2016-10-29 17:54:12 +0300

    C# I - homework 6 - 02. Not Divisible Numbers
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# I/homework_6_c_sharp_due_04.11.2016/02. Not Divisible Numbers/NotDividibleNumbers.cs
@@ -0,0 +1,45 @@
+﻿/**
+Not Divisible Numbers
+
+Description
+
+Write a program that reads from the console a positive integer N and prints all the numbers from 1 to N not divisible by 3 or 7, on a single line, separated by a space.
+
+Input
+
+Will always consists of one valid integer number - the number N.
+Output
+
+Should always consists of the numbers from 1 to N, which are not divisible by 3 or 7, separated by a whitespace.
+Constraints
+
+1 < N < 1500
+Time limit: 0.1s
+Memory limit: 16MB
+Sample tests
+
+Input	Output
+10	1 2 4 5 8 10
+3	1 2
+**/
+
+using System;
+using System.Text;
+
+class NotDividibleNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        StringBuilder builder = new StringBuilder("");
+        for (int i = 1; i <= n; i++)
+        {
+            if ((i % 3 != 0) && (i % 7 != 0))
+            {
+                if (builder.Length > 0) builder.Append(" ");
+                builder.Append(i);
+            }
+        }
+        Console.WriteLine(builder);
+    }
+}
