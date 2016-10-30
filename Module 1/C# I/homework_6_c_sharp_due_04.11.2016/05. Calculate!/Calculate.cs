a780ef41dd1c48db4520e42dbe0daa470c96d2c2
stoychevvasko <stoychev.vassil@gmail.com>
2016-10-30 12:04:39 +0200

    C#I - homework 6 - 05. Calculate!
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# I/homework_6_c_sharp_due_04.11.2016/05. Calculate!/Calculate.cs
@@ -0,0 +1,74 @@
+﻿/**
+Calculate!
+
+Description
+
+Write a program that, for a given two numbers N and x, calculates the sum S = 1 + 1!/x + 2!/x2 + … + N!/xN.
+
+Use only one loop. Print the result with 5 digits after the decimal point.
+Input
+
+On the first line you will receive one number - N.
+On the second line you will receive another number - x.
+Output
+
+Output only one number - the sum of the sequence for the given N and x.
+Constraints
+
+N will always be a valid integer between 2 and 10, inclusive.
+X will always be a valid floating-point number between 0.5 and 100
+Time limit: 0.1s
+Memory limit: 16MB
+Sample tests
+
+Input	Output
+3
+2	2.75000
+4
+3	2.07407
+5
+-4	0.75781
+**/
+
+using System;
+
+class Calculate
+{
+    static void Main()
+    {
+        uint n = uint.Parse(Console.ReadLine());
+        decimal x = decimal.Parse(Console.ReadLine());
+        decimal result = CalculateValues(n, x);
+        Console.WriteLine("{0:F5}", result);
+    }
+
+    private static decimal CalculateValues(uint intValue, decimal decimalValue)
+    {
+        decimal result = 1;
+        for (uint i = 1; i <= intValue; i++)
+        {
+            result += Factorial(i) / DecimalPower(decimalValue, i);
+        }
+        return result;
+    }
+
+    private static decimal DecimalPower(decimal decimalValue, uint pow)
+    {
+        decimal result = 1;
+        if (pow > 0)
+        {
+            for (int i = 0; i < pow; i++)
+            {
+                result *= decimalValue;
+            }
+        }
+        
+        return result;
+    }
+
+    private static uint Factorial(uint value)
+    {
+        if (value == 0 || value == 1) return 1;
+        else return Factorial(value - 1) * value;
+    }    
+}
