0977c204abff4387e7f7ef98f98aa3491f644255
stoychevvasko <stoychev.vassil@gmail.com>
2016-11-01 11:08:12 +0200

    C# I - homework 6 - 07. Calculate 3!
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# I/homework_6_c_sharp_due_04.11.2016/07. Calculate 3!/Calculate3.cs
@@ -0,0 +1,52 @@
+﻿/**
+Description
+
+In combinatorics, the number of ways to choose N different members out of a group of N different elements (also known as the number of combinations) is calculated by the following formula: formula For example, there are 2598960 ways to withdraw 5 cards out of a standard deck of 52 cards. Your task is to write a program that calculates N! / (K! * (N - K)!) for given N and K.
+
+Try to use only two loops.
+Input
+
+On the first line, there will be only one number - N
+On the second line, there will also be only one number - K
+Output
+
+On the only output line, write the result of the calculation for the provided N and K
+Constraints
+
+1 < K < N < 100
+Hint: overflow is possible
+Time limit: 0.1s
+Memory limit: 16MB
+Sample tests
+
+Input	Output
+3 
+2	3
+4 
+2	6
+10
+6	210
+52
+5	2598960
+**/
+
+using System;
+using System.Numerics;
+
+class Calculate3
+{
+    static void Main()
+    {
+        BigInteger n = BigInteger.Parse(Console.ReadLine());
+        BigInteger k = BigInteger.Parse(Console.ReadLine());
+
+        decimal result = (decimal)((Factorial(n)) / (Factorial(k) * Factorial(n - k)));
+        Console.WriteLine(result);
+    }
+
+    private static BigInteger Factorial(BigInteger value)
+    {
+        if (value == 0 || value == 1) return 1;
+        else return Factorial(value - 1) * value;
+    }
+}
