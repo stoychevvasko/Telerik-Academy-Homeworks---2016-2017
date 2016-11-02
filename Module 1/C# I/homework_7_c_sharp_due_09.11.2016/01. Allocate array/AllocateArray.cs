ecba1a6b80d8b6a204c6a626bb5ae8a3e121f9ae
stoychevvasko <stoychev.vassil@gmail.com>
2016-11-02 22:38:27 +0200

    C# I - homework 7 - 01. Allocate array
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# I/homework_7_c_sharp_due_09.11.2016/01. Allocate array/AllocateArray.cs
@@ -0,0 +1,45 @@
+﻿/**
+Allocate array
+
+Description
+
+Write a program that allocates array of N integers, initializes each element by its index multiplied by 5 and the prints the obtained array on the console.
+
+Input
+
+On the only line you will receive the number N
+Output
+
+Print the obtained array on the console.
+Each number should be on a new line
+Constraints
+
+1 <= N <= 20
+N will always be a valid integer number
+Time limit: 0.1s
+Memory limit: 16MB
+Sample tests
+
+Input	Output
+5	0
+5
+10
+15
+20
+**/
+
+using System;
+
+class AllocateArray
+{
+    static void Main()
+    { 
+        int n = int.Parse(Console.ReadLine());
+        int[] numbers = new int[n];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = i * 5;
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
