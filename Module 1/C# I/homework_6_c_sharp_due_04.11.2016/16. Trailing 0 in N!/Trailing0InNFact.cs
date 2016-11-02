b03d5c3c47fe087cb6e4f1646cbd450e5e4ac0fc
stoychevvasko <stoychev.vassil@gmail.com>
2016-11-02 21:38:16 +0200

    C# I - homework 6 - 16. Trailing 0 in N!
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# I/homework_6_c_sharp_due_04.11.2016/16. Trailing 0 in N!/Trailing0InNFact.cs
@@ -0,0 +1,45 @@
+﻿/**
+Trailing 0 in N!
+
+Description
+
+Write a program that calculates with how many zeroes the factorial of a given number N has at its end.
+
+Your program should work well for very big numbers, e.g. N = 100000.
+Input
+
+On the only input line, you will receive a single integer - the number N
+Output
+
+Output a single number - the count of trailing zeroes for the N!
+Constraints
+
+N will always be a valid positive integer number.
+Time limit: 0.1s
+Memory limit: 16MB
+Sample tests
+
+Input	Output	Explanation
+10	2	3628800
+20	4	2432902008176640000
+100000	24999	think why
+**/
+
+using System;
+using System.Numerics;
+
+class Trailing0InNFact
+{
+    static void Main()
+    {
+        BigInteger number = BigInteger.Parse(Console.ReadLine());
+        BigInteger trailingZeros = 0;
+        BigInteger div = 5;
+        while (number / div >= 1)
+        {
+            trailingZeros += (number / div);
+            div *= 5;
+        }
+        Console.WriteLine(trailingZeros);
+    }
+}
