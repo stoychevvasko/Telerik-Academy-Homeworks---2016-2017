0d145beb19ab5c7569df6b4bdfa057e91c236321
stoychevvasko <stoychev.vassil@gmail.com>
2016-11-06 17:36:09 +0200

    C# I - homework 7 - 15. Prime numbers
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# I/homework_7_c_sharp_due_09.11.2016/15. Prime numbers/PrimeNumbers.cs
@@ -0,0 +1,62 @@
+﻿/**
+Prime numbers
+
+Description
+
+Write a program that finds all prime numbers in the range [1 ... N]. Use the Sieve of Eratosthenes algorithm. The program should print the biggest prime number which is <= N.
+
+Input
+
+On the first line you will receive the number N
+Output
+
+Print the biggest prime number which is <= N
+Constraints
+
+2 <= N <= 10 000 000
+Time limit: 0.3s
+Memory limit: 64MB
+Sample tests
+
+Input	Output
+13	13
+126	113
+26	23
+**/
+
+using System;
+
+class PrimeNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int biggestPrimeUnderN = 0;
+        int[] allNumbers = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            allNumbers[i] = i + 1;
+        }
+        int sieve = 2;
+        bool finished = false;
+        while (!finished)
+        {
+            biggestPrimeUnderN = sieve;
+            for (int i = 2 * sieve - 1; i < n; i += sieve)
+            {
+                allNumbers[i] = 0;
+            }
+            finished = true;
+            for (int i = sieve; i < n; i++)
+            {
+                if (allNumbers[i] != 0)
+                {
+                    sieve = allNumbers[i];
+                    finished = false;
+                    break;
+                }
+            }
+        }
+        Console.WriteLine(biggestPrimeUnderN);
+    }
+}
