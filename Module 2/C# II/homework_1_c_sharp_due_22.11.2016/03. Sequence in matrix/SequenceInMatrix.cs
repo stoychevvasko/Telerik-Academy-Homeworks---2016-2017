1d2dac9bf06cbb0c25fa4b9f92241615fedf06a9
stoychevvasko <stoychev.vassil@gmail.com>
2016-11-19 16:51:57 +0200

    C# II - homework 1 - 03. Sequence in matrix 100/100 - refactored to use enumeration
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

--- Module 2/C# II/homework_1_c_sharp_due_22.11.2016/03. Sequence in matrix/SequenceInMatrix.cs
+++ Module 2/C# II/homework_1_c_sharp_due_22.11.2016/03. Sequence in matrix/SequenceInMatrix.cs
@@ -39,6 +39,8 @@ using System;
 
 class SequenceInMatrix
 {
+    enum Directions { Vertical = 0, Horizontal = 1, RightDiagonal = 2, LeftDiagonal = 3 };
+
     static void Main()
     {
         string input = Console.ReadLine();
@@ -53,67 +55,71 @@ class SequenceInMatrix
                 matrix[row, col] = line[col];
             }
         }
-
-        int bestSequenceLength = 1;
-        for (int row = 0; row < n; row++)
+        int maxSequence = 0;
+        foreach (Directions thisDirection in Enum.GetValues(typeof(Directions)))
         {
-            for (int col = 0; col < m; col++)
+            for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                int horizontalCount = CountHorizontal(matrix, row, col);
-                if (horizontalCount > bestSequenceLength) bestSequenceLength = horizontalCount;
-
-                int verticalCount = CountVertical(matrix, row, col);
-                if (verticalCount > bestSequenceLength) bestSequenceLength = verticalCount;
-
-                int rightDiagonalCount = CountRightDiagonal(matrix, row, col);
-                if (rightDiagonalCount > bestSequenceLength) bestSequenceLength = rightDiagonalCount;
-
-                int leftDiagonalCount = CountLeftDiagonal(matrix, row, col);
-                if (leftDiagonalCount > bestSequenceLength) bestSequenceLength = leftDiagonalCount;
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    int currentSequence = countNeighbours(matrix, row, col, thisDirection);
+                    if (currentSequence > maxSequence)
+                    {
+                        maxSequence = currentSequence;
+                    }
+                }
             }
         }
-
-        Console.WriteLine(bestSequenceLength);
+        Console.WriteLine(maxSequence);
     }
 
-
-    private static int CountHorizontal(string[,] matrix, int row, int col)
+    static int countNeighbours(string[,] matrix, int row, int col, Directions direction)
     {
-        if (row < 0 || col < 0 || row >= matrix.GetLength(0) || col >= matrix.GetLength(1))
+        switch (direction)
         {
-            // index out of bounds
-            return 0;
+            // check if the next element is out of range
+            case Directions.Vertical: if (row + 1 >= matrix.GetLength(0)) return 1; break;
+            case Directions.Horizontal: if (col + 1 >= matrix.GetLength(1)) return 1; break;
+            case Directions.RightDiagonal: if ((row + 1 >= matrix.GetLength(0)) || (col + 1 >= matrix.GetLength(1))) return 1; break;
+            case Directions.LeftDiagonal: if ((row + 1 >= matrix.GetLength(0)) || (col - 1 < 0)) return 1; break;
         }
-        else if (col + 1 < matrix.GetLength(1) && matrix[row, col] != matrix[row, col + 1])
+
+        switch (direction)
         {
-            // end of sequence
-            return 0;
+            // check if the next element is equal to the current one
+            case Directions.Vertical:
+                {
+                    if (matrix[row, col].CompareTo(matrix[row + 1, col]) == 0)
+                    {
+                        return (1 + countNeighbours(matrix, row + 1, col, Directions.Vertical));
+                    }
+                    break;
+                }
+            case Directions.Horizontal:
+                {
+                    if (matrix[row, col].CompareTo(matrix[row, col + 1]) == 0)
+                    {
+                        return (1 + countNeighbours(matrix, row, col + 1, Directions.Horizontal));
+                    }
+                    break;
+                }
+            case Directions.RightDiagonal:
+                {
+                    if (matrix[row, col].CompareTo(matrix[row + 1, col + 1]) == 0)
+                    {
+                        return (1 + countNeighbours(matrix, row + 1, col + 1, Directions.RightDiagonal));
+                    }
+                    break;
+                }
+            case Directions.LeftDiagonal:
+                {
+                    if (matrix[row, col].CompareTo(matrix[row + 1, col - 1]) == 0)
+                    {
+                        return (1 + countNeighbours(matrix, row + 1, col - 1, Directions.LeftDiagonal));
+                    }
+                    break;
+                }
         }
-        else
-        {
-            // index within bounds, next element equal - count and continue sequence
-            return 1 + CountHorizontal(matrix, row, col + 1);
-        }
-    }
-
-    private static int CountVertical(string[,] matrix, int row, int col)
-    {
-        if (row < 0 || col < 0 || row >= matrix.GetLength(0) || col >= matrix.GetLength(1)) return 0;
-        else if (row + 1 < matrix.GetLength(0) && matrix[row, col] != matrix[row + 1, col]) return 0;
-        else return 1 + CountVertical(matrix, row + 1, col);
-    }
-
-    private static int CountRightDiagonal(string[,] matrix, int row, int col)
-    {
-        if (row < 0 || col < 0 || row >= matrix.GetLength(0) || col >= matrix.GetLength(1)) return 0;
-        else if (row + 1 < matrix.GetLength(0) && col + 1 < matrix.GetLength(1) && matrix[row, col] != matrix[row + 1, col + 1]) return 0;
-        else return 1 + CountRightDiagonal(matrix, row + 1, col + 1);
-    }
-
-    private static int CountLeftDiagonal(string[,] matrix, int row, int col)
-    {
-        if (row < 0 || col < 0 || row >= matrix.GetLength(0) || col >= matrix.GetLength(1)) return 0;
-        else if (row + 1 < matrix.GetLength(0) && col - 1 >= 0 && matrix[row, col] != matrix[row + 1, col - 1]) return 0;
-        else return 1 + CountLeftDiagonal(matrix, row + 1, col - 1);
+        return 1;
     }
 }
