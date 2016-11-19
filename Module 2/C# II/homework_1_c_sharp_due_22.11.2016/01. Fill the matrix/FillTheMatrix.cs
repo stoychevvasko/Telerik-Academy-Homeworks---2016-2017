2010adf5e682104bc8b1b2069fe84399d8b3017b
stoychevvasko <stoychev.vassil@gmail.com>
2016-11-19 14:23:09 +0200

    C# II - homework 1 - 01. Fill the matrix (done)
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

--- Module 2/C# II/homework_1_c_sharp_due_22.11.2016/01. Fill the matrix/FillTheMatrix.cs
+++ Module 2/C# II/homework_1_c_sharp_due_22.11.2016/01. Fill the matrix/FillTheMatrix.cs
@@ -64,17 +64,29 @@ class FillTheMatrix
             case "a":
                 {
                     CreateMatrixA(matrix);
-                    PrintMatrix(matrix);
                     break;
                 }
-            case "b": break;
-            case "c": break;
-            case "d": break;
-            default: break;
+            case "b":
+                {
+                    CreateMatrixB(matrix);
+                    break;
+                }
+            case "c":
+                {
+                    CreateMatrixC(matrix);
+                    break;
+                }
+            case "d":
+                {
+                    CreateMatrixD(matrix);
+                    break;
+                }
+            default: throw new Exception("invalid choice!");
         }
+        PrintMatrix(matrix);
     }
 
-    static void CreateMatrixA(int[,] matrix)
+    private static void CreateMatrixA(int[,] matrix)
     {
         int value = 1;
         for (int col = 0; col < matrix.GetLength(1); col++)
@@ -87,9 +99,137 @@ class FillTheMatrix
         }
     }
 
+    private static void CreateMatrixB(int[,] matrix)
+    {
+        int value = 1;
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            if (col % 2 == 0)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    matrix[row, col] = value;
+                    value++;
+                }
+            }
+            else
+            {
+                for (int row = matrix.GetLength(0) - 1; row >= 0; row--)
+                {
+                    matrix[row, col] = value;
+                    value++;
+                }
+            }
+        }
+    }
+
+    private static void CreateMatrixC(int[,] matrix)
+    {
+        int value = 1;
+        int n = matrix.GetLength(0);
+        int row = n - 1;
+        int col = 0;
+        int lastDiagonalStartRow;
+        int lastDiagonalStartCol;
+        int diagonalCount = 1;
+        int diagonalDiscount = 1;
+        bool diagonalSwitch = false;
+
+        while (value <= n * n)
+        {
+            if (diagonalCount <= n)
+            {
+                lastDiagonalStartRow = row;
+                lastDiagonalStartCol = col;
+
+                for (int i = 1; i <= diagonalCount; i++)
+                {
+                    matrix[row, col] = value;
+                    value++;
+                    row++;
+                    col++;
+                }
+
+                row = lastDiagonalStartRow - 1;
+                col = lastDiagonalStartCol;
+                diagonalCount++;
+            }
+            else
+            {
+                if (!diagonalSwitch)
+                {
+                    lastDiagonalStartCol = 1;
+                    lastDiagonalStartRow = 0;
+                    col = 1;
+                    row = 0;
+                    diagonalSwitch = true;
+                }
+                else
+                {
+                    lastDiagonalStartRow = 0;
+                    lastDiagonalStartCol = col;
+                }
+
+                for (int i = 1; i <= diagonalCount - diagonalDiscount - 1; i++)
+                {
+                    while (col > n - 1)
+                    {
+                        col = n - 1;
+                    }
+
+                    matrix[row, col] = value;
+                    value++;
+
+                    row++;
+                    col++;
+                }
+
+                row = lastDiagonalStartRow;
+                col = lastDiagonalStartCol + 1;
+
+                diagonalCount++;
+                diagonalDiscount++;
+                diagonalDiscount++;
+            }
+        }
+    }
+
+    private static void CreateMatrixD(int[,] matrix)
+    {
+        int value = 1;
+        int n = matrix.GetLength(0);
+        int maxStep = (n % 2 == 0) ? (n / 2) : (n / 2 + 1);
+
+        for (int step = 0; step < maxStep; step++)
+        {
+            for (int row = 0 + step; row < n - step; row++)
+            {
+                matrix[row, 0 + step] = value;
+                value++;
+            }
+
+            for (int col = 1 + step; col < n - step; col++)
+            {
+                matrix[n - 1 - step, col] = value;
+                value++;
+            }
+
+            for (int row = n - 2 - step; row >= 0 + step; row--)
+            {
+                matrix[row, n - 1 - step] = value;
+                value++;
+            }
+
+            for (int col = n - 2 - step; col >= 1 + step; col--)
+            {
+                matrix[0 + step, col] = value;
+                value++;
+            }
+        }
+    }
+
     static void PrintMatrix(int[,] matrix)
     {
-
         for (int row = 0; row < matrix.GetLength(0); row++)
         {
             StringBuilder builder = new StringBuilder();
