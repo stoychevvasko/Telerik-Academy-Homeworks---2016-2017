69a38f7d51acec176db02ae8efb71e76e938b249
stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>
2016-11-22 17:15:56 +0200

    C# II - homework 2 - 01. Say hello
    Signed-off-by: stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>

---
+++ Module 2/C# II/homework_2_c_sharp_due_25.11.2016/01. Say Hello/SayHello.cs
@@ -0,0 +1,38 @@
+﻿/*
+Say Hello
+Description
+Write a method that asks the user for his name and prints Hello, <name>!. Write a program to test this method.
+
+Input
+On the first line you will receive a name
+
+Output
+Print Hello, <name>!
+
+Constraints
+Time limit: 0.1s
+Memory limit: 16MB
+
+Sample tests:
+Input
+Peter
+
+Output
+Hello, Peter!
+*/
+
+using System;
+
+class SayHello
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        PrintHello(input);
+    }
+
+    private static void PrintHello(string input)
+    {
+        Console.WriteLine("Hello, {0}!", input);
+    }
+}
