58c5f876eece6b763a32e36427d02616174d3c02
stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>
2016-11-23 23:59:33 +0200

    C# II - homework 2 - 07. Reverse number
    Signed-off-by: stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>

---
+++ Module 2/C# II/homework_2_c_sharp_due_25.11.2016/07. Reverse number/ReverseNumber.cs
@@ -0,0 +1,48 @@
+﻿/*
+Reverse number
+
+Description
+Write a method that reverses the digits of a given decimal number.
+
+Input
+On the first line you will receive a number
+
+Output
+Print the given number with reversed digits
+
+Constraints
+Time limit: 0.1s
+Memory limit: 16MB
+
+Sample tests
+
+Input
+256
+123.45
+
+Output
+652
+54.321
+*/
+
+using System;
+using System.Text;
+
+class ReverseNumber
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(Reverse(input));
+    }
+
+    private static decimal Reverse(string input)
+    {
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; i < input.Length; i++)
+        {
+            b.Insert(0, input[i]);
+        }
+        return decimal.Parse(b.ToString());
+    }
+}
