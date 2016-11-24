56ec296f2e596e7a55b13a1cd96c4899983d2d11
stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>
2016-11-24 07:48:16 +0200

    C# II - homework 2 - 10. N factorial
    Signed-off-by: stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>

---
+++ Module 2/C# II/homework_2_c_sharp_due_25.11.2016/10. N Factorial/NFactorial.cs
@@ -0,0 +1,50 @@
+﻿/*
+N Factorial
+
+Description
+
+Write a method that multiplies a number represented as an array of digits by a given integer number. 
+Write a program to calculate N!.
+
+Input
+On the first line you will receive the number N
+
+Output
+Print N!
+
+Constraints
+0 <= N <= 100
+Time limit: 0.1s
+Memory limit: 16MB
+
+Sample tests
+
+Input
+5
+
+Output
+
+120
+*/
+
+using System;
+using System.Numerics;
+
+class NFactorial
+{
+    static void Main()
+    {
+        int inputNum = int.Parse(Console.ReadLine());
+        BigInteger result = CalculateFactorial(inputNum);
+        Console.WriteLine(result);
+    }
+
+    static BigInteger CalculateFactorial(int number)
+    {
+        if (number == 0)
+        {
+            return 1;
+        }
+        return number * CalculateFactorial(number - 1);
+    }
+}
