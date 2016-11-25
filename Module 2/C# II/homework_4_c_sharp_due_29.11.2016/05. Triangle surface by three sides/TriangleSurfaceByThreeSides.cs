f1bd3711fba6c846c80924784d8e45a19f1efbd3
stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>
2016-11-25 20:38:13 +0200

    C# II - homework 4 - 05. Triangle surface by Heron formula
    Signed-off-by: stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>

---
+++ Module 2/C# II/homework_4_c_sharp_due_29.11.2016/05. Triangle surface by three sides/TriangleSurfaceByThreeSides.cs
@@ -0,0 +1,49 @@
+﻿/*
+Triangle surface by three sides
+
+Description
+Write program that calculates the surface of a triangle by given its three sides.
+
+Input
+On the first line you will receive the length of the first side of the triangle
+On the second line you will receive the length of the second side of the triangle
+On the third line you will receive the length of the third side of the triangle
+
+Output
+Print the surface of the rectangle with two digits of precision
+
+Constraints
+Input data describes a valid triangle
+Time limit: 0.1s
+Memory limit: 16MB
+
+Sample tests
+
+Input
+11
+12
+13
+
+Output
+61.48
+*/
+
+using System;
+
+class TriangleSurfaceByThreeSides
+{
+    static void Main()
+    {
+        double firstSide = double.Parse(Console.ReadLine());
+        double secondSide = double.Parse(Console.ReadLine());
+        double thirdSide = double.Parse(Console.ReadLine());
+        double result = CalcTriangleSurface(firstSide, secondSide, thirdSide);
+        Console.WriteLine("{0:f2}", result);
+    }
+
+    private static double CalcTriangleSurface(double a, double b, double c)
+    {
+        double s = (a + b + c) / 2.0d;
+        return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+}
