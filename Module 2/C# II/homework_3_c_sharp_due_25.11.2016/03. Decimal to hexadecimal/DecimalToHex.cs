387362deb3ef85857e76f8f576979fe239fcc32b
stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>
2016-11-25 14:05:24 +0200

    C# II - homework 3 - 03. Decimal to hexadecimal
    Signed-off-by: stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>

---
+++ Module 2/C# II/homework_3_c_sharp_due_25.11.2016/03. Decimal to hexadecimal/DecimalToHex.cs
@@ -0,0 +1,56 @@
+﻿/*
+Decimal to hexadecimal
+
+Description
+Write a program that converts a decimal number N to its hexadecimal representation.
+
+Input
+On the only line you will receive a decimal number - N 
+There will not be leading zeros
+
+Output
+Print the hexadecimal representation of N on a single line 
+There should not be leading zeros
+Use uppercase letters
+
+Constraints
+1 <= N <= 1018
+Time limit: 0.1s
+Memory limit: 16MB
+
+Sample tests
+
+Input
+19
+
+Output
+13
+*/
+
+using System;
+using System.Text;
+
+class DecimalToHex
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(ConvertDecToHex(input));
+    }
+
+    static public string ConvertDecToHex(string str)
+    {
+        string hexDigits = "0123456789ABCDEF";
+        long num = long.Parse(str);
+        StringBuilder result = new StringBuilder();
+
+        while (num > 0)
+        {
+            int index = (int)(num % 16L);
+            result.Insert(0, hexDigits[index]);
+            num /= 16L;
+        }
+
+        return result.ToString();
+    }
+}
