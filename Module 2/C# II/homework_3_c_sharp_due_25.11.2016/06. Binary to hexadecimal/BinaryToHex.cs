54a819bbd7e29b8ce5fb3bd0a95bb330bf9f2025
stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>
2016-11-25 15:40:09 +0200

    C# II - homework 3 - 06. Binary to hexadecimal
    Signed-off-by: stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>

---
+++ Module 2/C# II/homework_3_c_sharp_due_25.11.2016/06. Binary to hexadecimal/BinaryToHex.cs
@@ -0,0 +1,63 @@
+﻿/*
+Binary to hexadecimal
+
+Description
+Write a program to convert binary numbers to hexadecimal numbers (directly).
+
+Input
+On the only line you will receive a decimal number - N 
+There will not be leading zeros
+
+Output
+Print the its binary representation on a single line 
+There should not be leading zeros
+Use uppercase letters
+
+Constraints
+1 <= N <= 1018 = 110111100000101101101011001110100111011001000000000000000000(2)
+Time limit: 0.1s
+Memory limit: 16MB
+
+Sample tests
+
+Input
+10011
+
+Output
+13
+*/
+
+using System;
+using System.Text;
+
+class BinaryToHex
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(ConvertBinToHex(input));
+    }
+
+    static public string ConvertBinToHex(string str)
+    {
+        string hexDigits = "0123456789ABCDEF";
+        StringBuilder sourceString = new StringBuilder(str);
+        StringBuilder resultString = new StringBuilder();
+
+        while (sourceString.Length % 4 != 0)
+        {
+            sourceString.Insert(0, "0");
+        }
+
+        while (sourceString.Length > 0)
+        {
+            string halfByteStr = sourceString.ToString().Substring(0, 4);
+            sourceString.Remove(0, 4);
+
+            int halfByte = Convert.ToInt32(halfByteStr, 2);
+            resultString.Append(hexDigits[halfByte]);
+        }
+
+        return resultString.ToString();
+    }
+}
