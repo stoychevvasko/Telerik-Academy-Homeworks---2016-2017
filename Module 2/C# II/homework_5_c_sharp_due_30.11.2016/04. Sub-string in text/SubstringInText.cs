d3d6a2372d62a7773bb2a7e20151ee59ee5e101a
stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>
2016-11-25 22:59:29 +0200

    C# II - homework 5 - 04. Substring in text
    Signed-off-by: stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>

---
+++ Module 2/C# II/homework_5_c_sharp_due_30.11.2016/04. Sub-string in text/SubstringInText.cs
@@ -0,0 +1,46 @@
+﻿/*
+Sub-string in text
+
+Description
+Write a program that finds how many times a sub-string is contained in 
+a given text (perform case insensitive search).
+
+Input
+On the first line you will receive a string - the pattern
+On the second line you will receive a string - the text
+
+Output
+Print a number on a single line 
+The number of occurrences
+
+Constraints
+The length of the two strings will be <= 4096
+Time limit: 0.1s
+Memory limit: 16MB
+
+Sample tests
+
+Input
+in
+We are living in an yellow submarine. We don't have anything else. inside the submarine is very tight. So we are drinking all the day. We will move out of it in 5 days.
+
+Output
+9
+*/
+
+using System;
+
+class SubstringInText
+{
+    static void Main()
+    {
+        string pattern = Console.ReadLine();
+        string text = Console.ReadLine();
+        Console.WriteLine(CountOccurences(pattern.ToLower(), text.ToLower()));
+    }
+
+    private static int CountOccurences(string pattern, string text)
+    {
+        return (text.Length - text.Replace(pattern, String.Empty).Length) / pattern.Length;
+    }
+}
