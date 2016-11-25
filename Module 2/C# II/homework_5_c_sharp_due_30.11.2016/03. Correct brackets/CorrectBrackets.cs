651ccc9491f9464abb36e19f877997a4c70be7ab
stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>
2016-11-25 22:16:29 +0200

    C# II - homework 5 - 03. Correct brackets
    Signed-off-by: stoychev.vassil@gmail.com <stoychev.vassil@gmail.com>

---
+++ Module 2/C# II/homework_5_c_sharp_due_30.11.2016/03. Correct brackets/CorrectBrackets.cs
@@ -0,0 +1,44 @@
+﻿/*
+Correct brackets
+
+Description
+Write a program to check if in a given expression the ( and ) brackets are put correctly.
+
+Input
+On the only line you will receive an expression
+
+Output
+Print Correct if the brackets are correct 
+Incorrect otherwise
+
+Constraints
+1 <= length of expression <= 10000
+Time limit: 0.1s
+Memory limit: 16MB
+
+Sample tests
+
+Input
+((a+b)/5-d)
+)(a+b))
+
+Output
+Correct
+Incorrect
+*/
+
+using System;
+
+class CorrectBrackets
+{
+    static void Main()
+    {
+        string expression = Console.ReadLine();
+        Console.WriteLine(CheckBrackets(expression) ? "Correct" : "Incorrect");
+    }
+
+    private static bool CheckBrackets(string str)
+    {
+        return str.Split('(').Length == str.Split(')').Length;
+    }
+}
