4e33eb2f2c405cb9a38127ecba97042aa762638e
stoychevvasko <stoychev.vassil@gmail.com>
2016-11-29 14:22:03 +0200

    C# II - homework 5 - 15. ReplaceTags
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 2/C# II/homework_5_c_sharp_due_30.11.2016/15. Replace tags/ReplaceTags.cs
@@ -0,0 +1,38 @@
+﻿/*
+Replace tags
+
+Description
+Write a program that replaces in a HTML document given as string all the tags <a href="URL">TEXT</a> 
+with corresponding markdown notation [TEXT](URL).
+
+Input
+On the only input line you will receive a string
+
+Output
+Print the string with replaced tags
+
+Constraints
+Time limit: 0.1s
+Memory limit: 16MB
+
+Sample tests
+
+Input
+<p>Please visit <a href="http://academy.telerik.com">our site</a> to choose a training course. Also visit <a href="www.devbg.org">our forum</a> to discuss the courses.</p>
+
+Output
+<p>Please visit [our site](http://academy.telerik.com) to choose a training course. Also visit [our forum](www.devbg.org) to discuss the courses.</p>
+*/
+
+using System;
+using System.Text.RegularExpressions;
+
+class ReplaceTags
+{
+    static void Main()
+    {
+        string inputStr = Console.ReadLine();
+        string result = Regex.Replace(inputStr, @"<a.href=""([^>]*)"">([^<]*(?:(?!</a)<[^<]*)*)</a>", "[$2]($1)");
+        Console.WriteLine(result);
+    }
+}
