51e79c86e7bc727cca4189dd53e2218e2e0874e8
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-19 19:32:06 +0200

    C# III - homework 2 - 01. Structure (tbc)
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_2_due_04.01.2017/Problem 1. Structure/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Problem 1. Structure
+
+* Create a structure Point3D to hold a 3D-coordinate {X, Y, Z} in the Euclidian 3D space.
+* Implement the ToString() to enable printing a 3D point.
+*/
+
+namespace Problem_01
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
