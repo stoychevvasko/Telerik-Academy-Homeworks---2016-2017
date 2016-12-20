56cd4f1066458615a056649a4287af62a2a6e251
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-20 12:01:12 +0200

    C# III - homework 2 - 02. Static read-only field
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_2_due_04.01.2017/Problem 2. Static read-only field/Point3D.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+
+namespace Problem_02
+{
+    /// <summary>
+    /// Represents a geometric point in three-dimensional space.
+    /// </summary>
+    public struct Point3D
+    {
+        /// <summary>
+        /// Holds the x-axis coordinate value.
+        /// </summary>
+        private decimal x;
+
+        /// <summary>
+        /// Holds the y-axis coordinate value.
+        /// </summary>
+        private decimal y;
+
+        /// <summary>
+        /// Holds the z-axis coordinate value.
+        /// </summary>
+        private decimal z;
+
+        /// <summary>
+        /// Holds the start of a coordinate system in 3D space <see cref="Point3D"/> {0, 0, 0}.
+        /// </summary>
+        private static Point3D pointZero = new Point3D(0, 0, 0);
+
+        /// <summary>
+        /// Initializes a new <see cref="Point3D"/> struct.
+        /// </summary>
+        /// <param name="xValue">X-axis coordinate as <see cref="decimal"/></param>
+        /// <param name="yValue">Y-axis coordinate as <see cref="decimal"/></param>
+        /// <param name="zValue">Z-axis coordinate as <see cref="decimal"/></param>
+        public Point3D(decimal xValue, decimal yValue, decimal zValue)
+            : this()
+        {
+            this.X = xValue;
+            this.Y = yValue;
+            this.Z = zValue;
+        }
+
+        /// <summary>
+        /// Represents the x-axis coordinate value of a point in 3D Euclidian space.
+        /// </summary>
+        public decimal X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+
+        /// <summary>
+        /// Represents the y-axis coordinate value of a point in 3D Euclidian space.
+        /// </summary>
+        public decimal Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+
+        /// <summary>
+        /// Represents the z-axis coordinate value of a point in 3D Euclidian space.
+        /// </summary>
+        public decimal Z
+        {
+            get { return this.z; }
+            set { this.z = value; }
+        }
+
+        /// <summary>
+        /// Returns point zero (start of a coordinate system in 3D space).
+        /// </summary>
+        public static Point3D PointZero
+        {
+            get { return Point3D.pointZero; }
+        }
+
+        /// <summary>
+        /// Returns a string representation of 
+        /// </summary>
+        /// <returns>a <see cref="string"/></returns>
+        public override string ToString()
+        {
+            return new StringBuilder()
+                .Append("{ ")
+                .AppendFormat("{0}, {1}, {2}", this.X, this.Y, this.Z)
+                .Append(" }")
+                .ToString();
+
+        }
+    }
+}
---
+++ Module 1/C# III/homework_2_due_04.01.2017/Problem 2. Static read-only field/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Problem 2. Static read-only field
+
+* Add a private static read-only field to hold the start of the coordinate system – the point O{0, 0, 0}.
+* Add a static property to return the point O.
+*/
+
+using System;
+
+namespace Problem_02
+{
+    class Program
+    {
+        static void Main()
+        {
+            Point3D test = Point3D.PointZero;
+            Console.WriteLine(test);
+        }
+    }
+}
