a0f8677c1bd6999246b0025d2521e0fd0a0fe771
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-20 11:53:43 +0200

    C# III - homework 2 - 01. Structure
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

--- Module 1/C# III/homework_2_due_04.01.2017/Problem 1. Structure/Program.cs
+++ Module 1/C# III/homework_2_due_04.01.2017/Problem 1. Structure/Program.cs
@@ -5,12 +5,21 @@ Problem 1. Structure
 * Implement the ToString() to enable printing a 3D point.
 */
 
+using System;
+
 namespace Problem_01
 {
     class Program
     {
         static void Main()
         {
+            Point3D testZero = new Point3D(0, 0, 0);
+            Point3D testOne = new Point3D(1, 2, 3);
+            Point3D testTwo = new Point3D(-12.56M, -1337M, -12321.12321M);
+
+            Console.WriteLine(testZero);
+            Console.WriteLine(testOne);
+            Console.WriteLine(testTwo);
         }
     }
 }
