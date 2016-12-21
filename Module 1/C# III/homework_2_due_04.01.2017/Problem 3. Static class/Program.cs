fd24d2ab86d6d0f00c3cdd30a15633ca13d4a300
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-21 15:27:33 +0200

    C# III - homework 2 - 03. Static class
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_2_due_04.01.2017/Problem 3. Static class/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Problem 3. Static class
+
+* Write a static class with a static method to calculate the distance between two points in the 3D space.
+*/
+
+namespace Problem_03
+{
+    class Program
+    {
+        static void Main()
+        {
+            Point3D pointA = new Point3D(1, 1, 1);
+            Point3D pointB = Point3D.PointZero;
+            decimal distance = Calculate.Distance(pointA, pointB);
+
+            System.Console.WriteLine(distance);
+
+            // from Windows 10 Calculator (scientific mode) - Sqrt(3)
+            System.Console.WriteLine("1.7320508075688772935274463415059");
+        }
+    }
+}
