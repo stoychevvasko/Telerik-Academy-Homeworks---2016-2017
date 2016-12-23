a0fc3bd7627411d772e76aa17ac45e6cb7a79468
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-23 15:30:55 +0200

    C# III - homework 2 - 09. Matrix indexer
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_2_due_04.01.2017/Problem 9. Matrix indexer/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Problem 9. Matrix indexer
+
+* Implement an indexer this[row, col] to access the inner matrix cells.
+*/
+
+using System;
+
+namespace Problem_09
+{
+    class Program
+    {
+        static void Main()
+        {
+            GenericMatrix<float> testMatrix01 = new GenericMatrix<float>(5, 9);
+            float num = 0F;
+
+            Console.WriteLine("Float matrix:");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int row = 0; row < testMatrix01.Height; row++)
+            {
+                for (int col = 0; col < testMatrix01.Width; col++)
+                {
+                    testMatrix01[row, col] = num * (float)Math.PI;
+                    num++;
+                    Console.Write("{0, 9} ", testMatrix01[row, col]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
