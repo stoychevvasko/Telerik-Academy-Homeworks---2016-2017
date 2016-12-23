da2ce283b168d21ac8578eae1cc195c9cc863ed9
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-23 16:28:43 +0200

    C# III - homework 2 - 11. Version attribute
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_2_due_04.01.2017/Problem 11. Version attribute/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Problem 11. Version attribute
+
+* Create a [Version] attribute that can be applied to structures, classes, interfaces, enumerations and methods 
+and holds a version in the format major.minor (e.g. 2.11).
+* Apply the version attribute to a sample class and display its version at runtime.
+*/
+
+using System;
+using System.Reflection;
+
+namespace Problem_11
+{
+    [VersionAttribute(2, 22)]
+    public class TestClass
+    {
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var typesArray = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in typesArray)
+            {
+                Object[] attirbutes = type.GetCustomAttributes(false);
+
+                foreach (var attr in attirbutes)
+                {
+                    if (attr is VersionAttribute)
+                    {
+                        Console.WriteLine("{0} v.{1}.{2}", type.Name, ((VersionAttribute)attr).Major, ((VersionAttribute)attr).Minor);
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Module 1/C# III/homework_2_due_04.01.2017/Problem 11. Version attribute/VersionAttribute.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Problem_11
+{
+
+    [AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class | AttributeTargets.Interface
+    | AttributeTargets.Enum | AttributeTargets.Method, AllowMultiple = false)]
+
+    [VersionAttribute(1, 11)]
+    /// <summary>
+    /// Represents a version attribute for custom attributes.
+    /// </summary>
+    public class VersionAttribute : System.Attribute
+    {
+        // fields
+        /// <summary>
+        /// Holds the minor version attribute value.
+        /// </summary>
+        private int minor;
+
+        /// <summary>
+        /// Holds the major version attribute value.
+        /// </summary>
+        private int major;
+
+        // properties       
+        /// <summary>
+        /// Represents the major version attribute value.
+        /// </summary>
+        public int Major
+        {
+            get { return this.major; }
+            private set { this.major = value; }
+        }
+
+        /// <summary>
+        /// Represents the minor version attribute value.
+        /// </summary>
+        public int Minor
+        {
+            get { return this.minor; }
+            private set { this.minor = value; }
+        }
+
+        // constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VersionAttribute"/> class.
+        /// </summary>
+        /// <param name="major">Major version attribute parameter value.</param>
+        /// <param name="minor">Minor version attribute parameter value.</param>
+        public VersionAttribute(int major, int minor)
+        {
+            this.Major = major;
+            this.Minor = minor;
+        }
+    }
+}
