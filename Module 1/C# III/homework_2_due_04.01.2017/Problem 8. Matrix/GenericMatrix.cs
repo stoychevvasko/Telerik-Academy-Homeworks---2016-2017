24e02e3e1a611bb62c4834dab998129df5a39cd4
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-23 14:46:28 +0200

    C# III - homework 2 - 08. Generic matrix
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_2_due_04.01.2017/Problem 8. Matrix/GenericMatrix.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Problem_08
+{
+    /// <summary>
+    /// Represents a strongly typed 2-D matrix of objects that can be accessed by index. 
+    /// </summary>
+    /// <typeparam name="T">T is generic notation for base type <see cref="T"/></typeparam>
+    public class GenericMatrix<T>
+        where T : struct, IComparable, IComparable<T>, IConvertible
+    {
+        /// <summary>
+        /// Holds elements of the <see cref="T"/> type within this <see cref="GenericMatrix{T}"/> object.
+        /// </summary>
+        private T[,] data;
+
+        /// <summary>
+        /// Holds the max width value for this <see cref="GenericMatrix{T}"/> object.
+        /// </summary>
+        private int width;
+
+        /// <summary>
+        /// Holds the max height value for this <see cref="GenericMatrix{T}"/> object.
+        /// </summary>
+        private int height;
+
+        //public T[,] Data
+        //{            
+        //    get { return (T[,])this.data.Clone(); }
+        //    private set { this.data = value; }
+        //}
+
+        /// <summary>
+        /// Represents the max width value of a <see cref="GenericMatrix{T}"/> object.
+        /// </summary>
+        public int Width
+        {
+            get { return this.width; }
+            private set { this.width = value; }
+        }
+
+        /// <summary>
+        /// Represents the max height value of a <see cref="GenericMatrix{T}"/> object.
+        /// </summary>
+        public int Height
+        {
+            get { return this.height; }
+            private set { this.height = value; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericMatrix{T}"/> class.
+        /// </summary>
+        public GenericMatrix()
+            : this(3, 3)
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericMatrix{T}"/> class.
+        /// </summary>
+        /// <param name="width">Maximum width parameter.</param>
+        /// <param name="height">Maximum height parameter.</param>
+        public GenericMatrix(int width, int height)
+        {
+            this.data = new T[height, width];
+            this.Width = width;
+            this.Height = height;
+        }
+    }
+}
---
+++ Module 1/C# III/homework_2_due_04.01.2017/Problem 8. Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Problem 8. Matrix
+
+* Define a class Matrix<T> to hold a matrix of numbers (e.g. integers, floats, decimals).
+*/
+
+using System;
+
+namespace Problem_08
+{
+    class Program
+    {
+        static void Main()
+        {
+            // use constructor with dimension parameters
+            GenericMatrix<int> testInts = new GenericMatrix<int>(5, 12);
+            Console.WriteLine("testInts.Width: {0},    testInts.Height: {1}", testInts.Width, testInts.Height);
+            Console.WriteLine();
+
+            // use parameterless constructor
+            GenericMatrix<decimal> testDecimals = new GenericMatrix<decimal>();
+            Console.WriteLine("testDecimals.Width: {0},    testDecimals.Height: {1}", testDecimals.Width, testDecimals.Height);
+            Console.WriteLine();
+        }
+    }
+}
