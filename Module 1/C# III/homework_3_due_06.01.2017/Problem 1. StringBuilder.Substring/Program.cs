3839a216129daac70384713a1f6045e0f8b0f697
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-24 10:22:05 +0200

    C# III - homework 3 (tbc)
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_3_due_06.01.2017/Problem 1. StringBuilder.Substring/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Problem 1. StringBuilder.Substring
+
+* Implement an extension method Substring(int index, int length) for the class StringBuilder that returns 
+new StringBuilder and has the same functionality as Substring in the class String.
+*/
+
+namespace Problem_01
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
