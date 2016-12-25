fdf4b1fc22310bd6920a35d7e793890adf0526ca
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-25 19:12:21 +0200

    C# III - homework 3 - 01. Substring - StringBuilder extension method
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

--- Module 1/C# III/homework_3_due_06.01.2017/Problem 1. StringBuilder.Substring/Program.cs
+++ Module 1/C# III/homework_3_due_06.01.2017/Problem 1. StringBuilder.Substring/Program.cs
@@ -5,13 +5,52 @@ Problem 1. StringBuilder.Substring
 new StringBuilder and has the same functionality as Substring in the class String.
 */
 
+using System;
+using System.Text;
+
 namespace Problem_01
 {
     class Program
     {
         static void Main()
         {
+            string testString = "Lorem ispum";
+            StringBuilder testBuilder = new StringBuilder(testString);
 
+            Console.WriteLine("test 01");
+            Console.WriteLine(testString.Substring(0, 5));
+            Console.WriteLine(testBuilder.Substring(0, 5));
+            Console.WriteLine();
+
+            // Console.WriteLine("test 02");
+            // Console.WriteLine(testString.Substring(-1, 6));
+            // Console.WriteLine(testBuilder.Substring(-1, 6));
+            // Console.WriteLine();
+
+            // Console.WriteLine("test 03");
+            // Console.WriteLine(testString.Substring(0, 12));
+            // Console.WriteLine(testBuilder.Substring(0, 12));
+            // Console.WriteLine();
+
+            // Console.WriteLine("test 04");
+            // Console.WriteLine(testString.Substring(0, -3));
+            // Console.WriteLine(testBuilder.Substring(0, -3));
+            // Console.WriteLine();
+
+            Console.WriteLine("test 05");
+            Console.WriteLine(testString.Substring(5, 2));
+            Console.WriteLine(testBuilder.Substring(5, 2));
+            Console.WriteLine();
+
+            Console.WriteLine("test 06");
+            Console.WriteLine(testString.Substring(5, 0));
+            Console.WriteLine(testBuilder.Substring(5, 0));
+            Console.WriteLine();
+
+            Console.WriteLine("test 07");
+            Console.WriteLine(testString.Substring(1, 5));
+            Console.WriteLine(testBuilder.Substring(1, 5));
+            Console.WriteLine();
         }
     }
 }
---
+++ Module 1/C# III/homework_3_due_06.01.2017/Problem 1. StringBuilder.Substring/StringBuilderExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace Problem_01
+{
+    /// <summary>
+    /// Provides extended functionality for the <see cref="StringBuilder"/> class.
+    /// </summary>
+    public static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Retrieves a substring from this instance as <see cref="StringBuilder"/>. The substring starts at a specified character position and has a specified length.
+        /// </summary>
+        /// <param name="source">A <see cref="StringBuilder"/> object.</param>
+        /// <param name="startIndex">The zero-based starting character position of a substring in this instance.</param>
+        /// <param name="length">The number of characters in the substring.</param>
+        /// <returns>A string that is equivalent to the substring of length length that begins at startIndex in this instance, or System.String.Empty if startIndex is equal to the length of this instance and length is zero.</returns>
+        public static StringBuilder Substring(this StringBuilder source, int startIndex, int length)
+        {
+            StringBuilder result = new StringBuilder();
+
+            if (startIndex < 0)
+            {
+                throw new System.ArgumentOutOfRangeException("StartIndex cannot be less than zero.");
+            }
+
+            if (length < 0)
+            {
+                throw new System.ArgumentOutOfRangeException("Length cannot be less than zero.");
+            }
+
+            if (startIndex > source.Length - 1 || startIndex + length > source.Length)
+            {
+                throw new System.ArgumentOutOfRangeException("StartIndex and length must refer to a location within the string.");
+            }
+
+            for (int i = startIndex; i < startIndex + length; i++)
+            {
+                result.Append(source[i]);
+
+                if (i >= source.Length - 1)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
