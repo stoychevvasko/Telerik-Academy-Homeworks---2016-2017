fa71818759b1c95e902c844f6220a2baf78e1a7d
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-26 17:26:47 +0200

    C# III - homework 3 - 02. IEnumerable extensions
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_3_due_06.01.2017/Problem 2. IEnumerable extensions/IEnumerableExtensions.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_02
+{
+    /// <summary>
+    /// Provides extended functionality for <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Retrieves the sum of a <see cref="IEnumerable{T}"/> collection.
+        /// </summary>
+        /// <typeparam name="T">A generic type.</typeparam>
+        /// <param name="collection">An <see cref="IEnumerable{T}"/> collection.</param>
+        /// <returns>Sum of the generic collection.</returns>
+        public static T Sum<T>(this IEnumerable<T> collection)
+        {
+            dynamic result = 0;
+
+            foreach (var item in collection)
+            {
+                result += (dynamic)item;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Retrieves the product of a <see cref="IEnumerable{T}"/> collection.
+        /// </summary>
+        /// <typeparam name="T">A generic type.</typeparam>
+        /// <param name="collection">An <see cref="IEnumerable{T}"/> collection.</param>
+        /// <returns>Product of the generic collection.</returns>
+        public static T Product<T>(this IEnumerable<T> collection)
+        {
+            dynamic result = 1;
+
+            foreach (var item in collection)
+            {
+                result *= (dynamic)item;
+
+                if (result == 0)
+                {
+                    return result;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Retrieves the minimal element of a <see cref="IEnumerable{T}"/> collection.
+        /// </summary>
+        /// <typeparam name="T">A generic type.</typeparam>
+        /// <param name="collection">An <see cref="IEnumerable{T}"/> collection.</param>
+        /// <returns>Minimal element of the generic collection.</returns>
+        public static T Min<T>(this IEnumerable<T> collection)
+        {
+            dynamic result = long.MaxValue;
+
+            foreach (var item in collection)
+            {
+                if (item < result)
+                    result = item;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Retrieves the maximal element of a <see cref="IEnumerable{T}"/> collection.
+        /// </summary>
+        /// <typeparam name="T">A generic type.</typeparam>
+        /// <param name="collection">An <see cref="IEnumerable{T}"/> collection.</param>
+        /// <returns>Maximal element of the generic collection.</returns>
+        public static T Max<T>(this IEnumerable<T> collection)
+        {
+            dynamic result = long.MinValue;
+
+            foreach (var item in collection)
+            {
+                if (item > result)
+                    result = item;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Retrieves the average element of a <see cref="IEnumerable{T}"/> collection.
+        /// </summary>
+        /// <typeparam name="T">A generic type.</typeparam>
+        /// <param name="collection">An <see cref="IEnumerable{T}"/> collection.</param>
+        /// <returns>Average element of the generic collection.</returns>
+        public static T Average<T>(this IEnumerable<T> collection)
+        {
+            dynamic result = 0, counter = 0;
+
+            foreach (var item in collection)
+            {
+                result += item;
+                counter++;
+            }
+
+            if (counter == 0)
+            {
+                throw new ArgumentException("Empty collection!");
+            }
+
+            return result / counter;
+        }
+    }
+}
---
+++ Module 1/C# III/homework_3_due_06.01.2017/Problem 2. IEnumerable extensions/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Problem 2. IEnumerable extensions
+
+*Implement a set of extension methods for IEnumerable<T> that implement the following group functions: 
+sum, product, min, max, average.
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Problem_02
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<decimal> testCollection = new HashSet<decimal> { -25, 1, 2, 3, -40, 4, 5, 150, -10, -11, -12, -13, -14 };
+
+            Console.WriteLine("SUM: {0}", testCollection.Sum<decimal>());
+            Console.WriteLine("PRODUCT: {0:N}", testCollection.Product<decimal>());
+            Console.WriteLine("MIN: {0}", testCollection.Min<decimal>());
+            Console.WriteLine("MAX: {0}", testCollection.Max<decimal>());
+            Console.WriteLine("AVG: {0}", testCollection.Average<decimal>());
+            
+            Console.WriteLine();
+        }
+    }
+}
