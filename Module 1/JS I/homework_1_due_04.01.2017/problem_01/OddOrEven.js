c0b7a32a83f2c64c9952f375a12510dacd098ef1
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-27 21:58:54 +0200

    JS I - homework 1 - 01. Odd or Even
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/JS I/homework_1_due_04.01.2017/problem_01/OddOrEven.js
@@ -0,0 +1,9 @@
+
+// Problem 1: Odd or Even
+
+function solve(args) {
+	var n = +args[0];
+    var result = ((n % 2) === 0) ? ('even ' + n) : ('odd ' + n);    
+    console.log(result);
+	return result;
+}
