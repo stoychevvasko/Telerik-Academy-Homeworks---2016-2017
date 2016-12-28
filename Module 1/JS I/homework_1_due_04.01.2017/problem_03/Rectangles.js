e94b1fc5065f32f4fbf0ca4827cbe1d9b524aa00
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-28 11:01:41 +0200

    JS I - homework 1 - 03. Rectangles
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/JS I/homework_1_due_04.01.2017/problem_03/Rectangles.js
@@ -0,0 +1,10 @@
+// // Problem 3: Rectangles
+
+function solve(args) {
+	var a = +args[0],
+        b = +args[1];
+    var perimeter = 2 * (a + b);
+    var surface = a * b;
+    var result = surface.toFixed(2) + ' ' + perimeter.toFixed(2);
+    return result;
+}
