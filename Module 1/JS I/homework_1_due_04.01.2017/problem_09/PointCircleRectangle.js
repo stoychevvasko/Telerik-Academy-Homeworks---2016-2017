9ac2e4135646db3ba7e675b2aafa74f6aa50d1d6
stoychevvasko <stoychev.vassil@gmail.com>
2016-12-29 12:16:33 +0200

    JS I - homework 1 - 09. Point, circle, rectangle
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/JS I/homework_1_due_04.01.2017/problem_09/PointCircleRectangle.js
@@ -0,0 +1,13 @@
+// Problem 9: Point, Circle, Rectangle
+
+function solve(args) {
+    var x = +args[0];
+    var y = +args[1];    
+    var circle = [1.0, 1.0, 1.5];    
+    var rectangle = [[-1.0, 5.0], [1.0, -1.0]];
+    var pointToCircleCenter = Math.sqrt(Math.pow((x + (0 - circle[0])), 2) + Math.pow((y + (0 - circle[1])), 2));
+    var inCircle = (pointToCircleCenter <= circle[2]) ? "inside circle " : "outside circle ";
+    var inRectangle = (( x >= rectangle[0][0] && x <= rectangle[0][1] ) &&
+                       ( y <= rectangle[1][0] && y >= rectangle[1][1] )) ? "inside rectangle" : "outside rectangle";
+    return (inCircle + inRectangle);    
+}
