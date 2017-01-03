cc28b7656567efa13ca367bd8868230003dcef27
stoychevvasko <stoychev.vassil@gmail.com>
2017-01-03 18:56:26 +0200

    C# III - homework 3 - 06. Divisible by 7 and 3
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_3_due_06.01.2017/Problem 6. Divisible by 7 and 3/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Problem 6. Divisible by 7 and 3
+
+* Write a program that prints from given array of integers all numbers that are divisible by 7 and 3. 
+Use the built-in extension methods and lambda expressions. Rewrite the same with LINQ.
+*/
+
+using System;
+using System.Linq;
+
+
+namespace Problem_06
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] allNums = new int[1000];
+
+            for (int i = 1; i < 1001; i++)
+            {
+                allNums[i - 1] = i;
+            }
+
+            // Lambda expression
+
+            var resultLambda = allNums.Where(x => x % 7 == 0 && x % 3 == 0).ToArray();
+
+            Console.WriteLine("with Lambda expression:");
+
+            foreach (var item in resultLambda)
+            {
+                Console.Write("{0, 3} ", item);
+            }
+
+            Console.WriteLine();
+
+            // LINQ
+
+            var resultLinq =
+                from num in allNums
+                where num % 7 == 0 && num % 3 == 0
+                select num;
+
+            Console.WriteLine();
+            Console.WriteLine("with LINQ query:");
+
+            foreach (var item in resultLinq)
+            {
+                Console.Write("{0, 3} ", item);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
