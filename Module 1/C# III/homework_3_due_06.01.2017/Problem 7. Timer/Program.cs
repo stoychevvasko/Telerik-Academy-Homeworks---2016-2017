ad0b4453859ae7a5e2680743d289667e2aad3495
stoychevvasko <stoychev.vassil@gmail.com>
2017-01-03 19:23:03 +0200

    C# III - homework 3 - 07. Timer
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_3_due_06.01.2017/Problem 7. Timer/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Problem 7. Timer
+
+* Using delegates write a class Timer that can execute certain method at each t seconds.
+*/
+
+using System;
+
+namespace Problem_07
+{
+    class Program
+    {
+        static void TestMethod()
+        {
+            Console.WriteLine("I did something using a timer on {0}.", DateTime.Now.ToString("d MMMM yyyy @ HH:mm:ss"));
+            Console.WriteLine();
+        }
+
+        static void Main()
+        {
+            Timer timer = new Timer();
+            timer.method = TestMethod;
+            timer.Play(2, 10);
+        }
+    }
+}
---
+++ Module 1/C# III/homework_3_due_06.01.2017/Problem 7. Timer/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Problem_07
+{
+    /// <summary>
+    /// Executes a method at each t-second interval (intervalInSeconds).
+    /// </summary>
+    public class Timer
+    {
+        public delegate void MethodFed();
+
+        public MethodFed method;
+
+        public void Play(int intervalInSeconds, int durationInSeconds)
+        {
+            DateTime start = DateTime.Now;
+            DateTime end = start.AddSeconds(durationInSeconds);
+
+            while (start <= end)
+            {
+                method();
+                Thread.Sleep(intervalInSeconds * 1000);
+                start = DateTime.Now;
+            }
+        }
+    }
+}
