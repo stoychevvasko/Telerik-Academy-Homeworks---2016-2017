e8d81a01619ebe86fa0730c35350d14ef7fd3bbe
stoychevvasko <stoychev.vassil@gmail.com>
2017-01-03 21:12:26 +0200

    C# III - homework 3 - 09. Student groups
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_3_due_06.01.2017/Problem 9. Student groups/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Problem 9. Student groups
+
+* Create a class Student with properties FirstName, LastName, FN, Tel, Email, Marks (a List), GroupNumber.
+* Create a List<Student> with sample students. Select only the students that are from group number 2.
+* Use LINQ query. Order the students by FirstName.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_09
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Student> sampleStudents = new List<Student>();
+            sampleStudents.Add(new Student("Joro", "Jorov", "123456789", "0888 888 888", "joro@joromail.com", new List<int> { 2, 3, 4, 5, 6 }, 1));
+            sampleStudents.Add(new Student("Pepi", "Pepiev", "123456790", "0888 888 889", "pepi@pepimail.org", new List<int> { 6, 6, 4, 5, 6 }, 1));
+            sampleStudents.Add(new Student("Niki", "Nikolaev", "123456791", "0888 777 888", "Niki@abv.bg", new List<int> { 2, 2, 4, 5, 5 }, 2));
+            sampleStudents.Add(new Student("Lili", "Lilieva", "123456792", "0888 668 888", "lili@mail.bg", new List<int> { 6, 6, 6, 5, 6 }, 2));
+            sampleStudents.Add(new Student("Asya", "Asieva", "123456793", "0889 999 888", "asya@gmail.com", new List<int> { 6, 5, 6, 5, 5 }, 2));
+
+            var linqQuery =
+                from student in sampleStudents
+                where student.GroupNumber == 2
+                orderby student.FirstName ascending, student.LastName ascending
+                select student;
+
+            Console.WriteLine("Students from Group 2 are:");
+            Console.WriteLine();
+
+            foreach (var item in linqQuery)
+            {
+                Console.WriteLine("{0} {1}", item.FirstName, item.LastName);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
