5377228bd3b70dec5ab5c0aa6e0dd94eec05ffcc
stoychevvasko <stoychev.vassil@gmail.com>
2017-01-04 11:44:55 +0200

    C# III - homework 3 - 17. Longest string
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 1/C# III/homework_3_due_06.01.2017/Problem 17. Longest string/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Problem 17. Longest string
+
+* Write a program to return the string with maximum length from an array of strings.
+* Use LINQ.
+*/
+
+using System;
+using System.Linq;
+
+namespace Problem_17
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] array = new string[] { "short", "mini", "this_is_the_longest_string_here", "longer", "this_one_is_long" };
+
+            Console.WriteLine("The longest string is:");
+            Console.WriteLine();
+
+            string result = (from str in array orderby str.Length select str).Last();
+
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+        }
+    }
+}
