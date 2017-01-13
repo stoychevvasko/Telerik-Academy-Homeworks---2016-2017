070478305d3c575e122b10794b2c013d69a6433d
stoychevvasko <stoychev.vassil@gmail.com>
2017-01-13 18:30:56 +0200

    JS I - homework 6 - 02. Remove elements
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

--- Module 1/JS I/homework_6_due_13.01.2017/problem_03/YoungestPerson.js
+++ Module 1/JS I/homework_6_due_13.01.2017/problem_03/YoungestPerson.js
@@ -1,4 +1,35 @@
 // Problem 03: Youngest person - BGCoder format
-function solve(args) {
-	
+function solve(args) {	
+    var input = args;
+    var people = [];
+ 
+    // Create a person constructor
+    function constructPerson(firstName, lastName, age) {
+        return {
+            firstname: firstName,
+            lastname: lastName,
+            age: parseInt(age)
+        };
+    }
+ 
+    // Create a function to compare age for a given array of Person objects
+    function getYoungest(people) {
+        var youngestPerson = people[0];
+        for (var i = 1; i < people.length; i += 1) {
+            if (people[i].age < youngestPerson.age) {
+                youngestPerson = people[i];
+            }
+        }
+        return youngestPerson;
+    }
+ 
+    // Construct an array of Person objects for an unknown input size
+    while(input.length > 0) {
+        var person = constructPerson.apply(this, (input.slice(0, 3)));
+        people.push(person);
+        input.splice(0, 3);
+    }
+ 
+    youngestPerson = getYoungest(people);
+    console.log(youngestPerson.firstname + ' ' + youngestPerson.lastname); 
 }
