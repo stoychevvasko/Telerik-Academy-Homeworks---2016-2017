8a7fd7e5fb37e6fde069badefeb947dfb11496ff
stoychevvasko <stoychev.vassil@gmail.com>
2017-02-16 13:51:24 +0200

    Unit Testing - exam preparation [...]
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 2/Unit Testing/exam_preparation/IntergalacticTravel.Tests/ResourcesFactoryTests/GetResources_Should.cs
@@ -0,0 +1,36 @@
+﻿namespace IntergalacticTravel.Tests.ResourcesFactoryTests
+{
+    using NUnit.Framework;
+    using System.Linq;
+
+    [TestFixture]
+    public class GetResources_Should
+    {
+        [Test]
+        [TestCase("create resources gold(20) silver(30) bronze(40)")]
+        [TestCase("create resources gold(20) bronze(40) silver(30)")]
+        [TestCase("create resources silver(30) bronze(40) gold(20)")]
+        [TestCase("create resources silver(30) gold(20) bronze(40)")]
+        [TestCase("create resources bronze(40) gold(20) silver(30)")]
+        [TestCase("create resources bronze(40) silver(30) gold(20)")]
+        public void ReturnProperNewResourcesObject_WhenCorrectPropertiesPassed(string command)
+        {
+            // Arrange
+            var factory = new ResourcesFactory();
+            var properties = command.Split(' ').Where(s => (s.Split('(')[0].ToLower() == "gold") ||
+            (s.Split('(')[0].ToLower() == "silver") || (s.Split('(')[0].ToLower() == "bronze"));
+
+            foreach (var item in properties)
+            {
+
+            }
+
+
+            // Act
+            var actual = factory.GetResources(command);
+
+            // Assert
+            Assert.That(actual, Is.InstanceOf<Resources>());            
+        }
+    }
+}
--- Module 2/Unit Testing/exam_preparation/IntergalacticTravel.Tests/UnitsFactoryTests/GetUnit_Should.cs
+++ Module 2/Unit Testing/exam_preparation/IntergalacticTravel.Tests/UnitsFactoryTests/GetUnit_Should.cs
@@ -1,15 +1,9 @@
-﻿using IntergalacticTravel.Exceptions;
-using Moq;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IntergalacticTravel.Tests.UnitsFactoryTests
+﻿namespace IntergalacticTravel.Tests.UnitsFactoryTests
 {
-    [TextFixture]
+    using IntergalacticTravel.Exceptions;
+    using NUnit.Framework;
+
+    [TestFixture]
     public class GetUnit_Should
     {
         [Test]        
