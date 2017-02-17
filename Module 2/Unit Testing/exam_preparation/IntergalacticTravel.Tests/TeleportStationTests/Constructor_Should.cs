bcaba972aa20aa23344e49d95f514c32121f6805
stoychevvasko <stoychev.vassil@gmail.com>
2017-02-17 20:46:54 +0200

    Unit Testing - Intergalactic Travel [...]
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 2/Unit Testing/exam_preparation/IntergalacticTravel.Tests/TeleportStationTests/Constructor_Should.cs
@@ -0,0 +1,28 @@
+﻿namespace IntergalacticTravel.Tests.TeleportStationTests
+{
+    using IntergalacticTravel.Contracts;
+    using Moq;
+    using NUnit.Framework;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void SetAppropriateValues_WhenNewTeleportStationIsCreatedWithValidParametersPassed()
+        {
+            // Arrange
+            var businessOwnerStub = new Mock<IBusinessOwner>().Object;
+            var galacticMapStub = new Mock<IEnumerable<IPath>>().Object;
+            var locationStub = new Mock<ILocation>().Object;
+
+            // Act
+            var actual = new TeleportStation_Fake(businessOwnerStub, galacticMapStub, locationStub);
+
+            // Assert
+            Assert.That(actual.Owner, Is.EqualTo(businessOwnerStub));
+            Assert.That(actual.GalacticMap, Is.EqualTo(galacticMapStub));
+            Assert.That(actual.Location, Is.EqualTo(locationStub));
+        }
+    }
+}
---
+++ Module 2/Unit Testing/exam_preparation/IntergalacticTravel.Tests/TeleportStationTests/TeleportStation_Fake.cs
@@ -0,0 +1,37 @@
+﻿namespace IntergalacticTravel.Tests.TeleportStationTests
+{
+    using System.Collections.Generic;
+    using IntergalacticTravel.Contracts;
+
+    public class TeleportStation_Fake : TeleportStation
+    {
+        public TeleportStation_Fake(IBusinessOwner owner, IEnumerable<IPath> galacticMap, ILocation location)
+            : base(owner, galacticMap, location)
+        {
+        }
+
+        public IBusinessOwner Owner
+        {
+            get
+            {
+                return this.owner;
+            }
+        }
+
+        public IEnumerable<IPath> GalacticMap
+        {
+            get
+            {
+                return this.galacticMap;
+            }
+        }
+
+        public ILocation Location
+        {
+            get
+            {
+                return this.location;
+            }
+        }
+    }
+}
