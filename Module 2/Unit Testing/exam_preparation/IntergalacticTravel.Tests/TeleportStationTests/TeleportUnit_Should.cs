d21cd21b8d544640cd9a46ab50431cfb0e000228
stoychevvasko <stoychev.vassil@gmail.com>
2017-02-18 16:18:13 +0200

    Unit Testing - IntergalacticTravel [...]
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 2/Unit Testing/exam_preparation/IntergalacticTravel.Tests/TeleportStationTests/TeleportUnit_Should.cs
@@ -0,0 +1,29 @@
+﻿namespace IntergalacticTravel.Tests.TeleportStationTests
+{
+    using Contracts;
+    using Moq;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class TeleportUnit_Should
+    {
+        [Test]
+        public void ThrowArgumentNullException_WhenCalledWithUnitToTeleportParameterNull()
+        {
+            // Arrange
+            var businessOwnerStub = new Mock<IBusinessOwner>().Object;
+            var galacticMapStub = new Mock<IEnumerable<IPath>>().Object;
+            var locationStub = new Mock<ILocation>().Object;
+            var actual = new TeleportStation(businessOwnerStub, galacticMapStub, locationStub);
+
+            // Act
+            var exception = Assert.Throws<ArgumentNullException>(() => actual.TeleportUnit(null, locationStub));
+
+            // Assert
+            Assert.That(() => actual.TeleportUnit(null, locationStub), Throws.ArgumentNullException);
+            StringAssert.Contains("unitToTeleport", exception.Message);
+        }
+    }
+}
