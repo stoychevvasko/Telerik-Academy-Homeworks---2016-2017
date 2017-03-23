4e6820edc9c3123e4729e52aef2952ce4dc144c3
stoychevvasko <stoychev.vassil@gmail.com>
2017-03-23 18:25:29 +0200

    JavaScript UI & DOM - HTML Templates - homework due 24.03.2017 - Task 3. ListView
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

--- Module 2/JS III - UI & DOM/05. HTML Templates - homework - due 24.03.2017/tasks/task-3.js
+++ Module 2/JS III - UI & DOM/05. HTML Templates - homework - due 24.03.2017/tasks/task-3.js
@@ -1,7 +1,14 @@
 function solve(){
   return function(){
     $.fn.listview = function(data){
-      
+      var $this = $(this);
+      var id = '#' + $this.attr('data-template');
+      var template = $(id).html();
+      var html = handlebars.compile(template);
+      for (var i = 0; i < data.length; i += 1)
+      {
+        $this.append(html(data[i]));
+      }
     };
   };
 }
