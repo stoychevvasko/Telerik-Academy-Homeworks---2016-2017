54048ba5fa8bae6b87015ea76a373b430ed0426b
stoychevvasko <stoychev.vassil@gmail.com>
2017-03-23 18:09:09 +0200

    JavaScript UI & DOM - HTML Templates - homework due 24.03.2017 - Task 2. Animals & Batman
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

--- Module 2/JS III - UI & DOM/05. HTML Templates - homework - due 24.03.2017/tasks/task-2.js
+++ Module 2/JS III - UI & DOM/05. HTML Templates - homework - due 24.03.2017/tasks/task-2.js
@@ -1,19 +1,48 @@
-/* globals $ */
-
 function solve() {
-  
   return function (selector) {
-    var template = ''; /* insert the template here as a string
-        example:
-        var template =
-          '<ul>' +
-            '{{#students}}' +
-            '<li>' +
-              '{{name}}' +
-            '</li>' +
-            '{{/students}}' +
-          '</ul>';
-    */
+    var data = {
+      animals: [{
+        name: 'Lion',
+        url: 'https://susanmcmovies.files.wordpress.com/2014/12/the-lion-king-wallpaper-the-lion-king-2-simbas-pride-4685023-1024-768.jpg'
+      }, {
+        name: 'Turtle',
+        url: 'http://www.enkivillage.com/s/upload/images/a231e4349b9e3f28c740d802d4565eaf.jpg'
+      }, {
+        name: 'Dog'              
+      }, {
+        name: 'Cat',
+        url: 'http://i.imgur.com/Ruuef.jpg'
+      }, {
+        name: 'Dog Again'              
+      }] 
+    }
+
+    var defaultUrl = 'http://cdn.playbuzz.com/cdn/3170bee8-985c-47bc-bbb5-2bcb41e85fe9/d8aa4750-deef-44ac-83a1-f2b5e6ee029a.jpg';
+
+    var template = 
+      '<div class="container">' + 
+        '<h1>' + 'Animals' + '</h1>' + 
+        '<ul class="animals-list">' + 
+          '{{#animals}}' + 
+            '<li>' + 
+              '<a href="' + 
+                '{{#if url}}' + 
+                  '{{url}}' + 
+                '{{else}}' + 
+                  defaultUrl + 
+                '{{/if}}">' + 
+                '{{#if url}}' + 
+                  'See a {{name}}' + 
+                '{{else}}' + 
+                  'No link for ' + 
+                    '{{name}}' + 
+                  ', here is Batman!' + 
+                '{{/if}}' + 
+              '</a>' +  
+            '</li>' + 
+          '{{/animals}}' + 
+        '</ul>' + 
+      '</div>';
 
     $(selector).html(template);
   };
