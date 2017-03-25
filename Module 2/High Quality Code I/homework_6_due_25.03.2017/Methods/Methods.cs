5087a76d3ab21ba8ba7611f9854a27f739c904d0
stoychevvasko <stoychev.vassil@gmail.com>
2017-03-25 13:34:05 +0200

    High Quality Code I - High Quality Methods - homework 6 due 25.03.2017 - Task 1. Quality Methods
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

--- Module 2/High Quality Code I/homework_6_due_25.03.2017/Methods/Methods.cs
+++ Module 2/High Quality Code I/homework_6_due_25.03.2017/Methods/Methods.cs
@@ -3,10 +3,11 @@ namespace Methods
 {
     using System;
 
-    public class Methods
+    /// <summary>Task 1.</summary>
+    internal class Methods
     {
-        /// <summary>Calculates the surface area of a 2-dimensional triangle shape.</summary><param name="a">A triangle side.</param><param name="b">A triangle side.</param><param name="c">A triangle side.</param><returns>The surface area calculated.</returns>
-        static double CalcTriangleArea(double a, double b, double c)
+        /// <summary>Calculates the surface area of a 2-dimensional triangle shape.</summary><param name="a">A triangle side.</param><param name="b">B triangle side.</param><param name="c">C triangle side.</param><returns>The surface area calculated.</returns>
+        public static double CalcTriangleArea(double a, double b, double c)
         {
             if (a <= 0 || b <= 0 || c <= 0)
             {
@@ -20,7 +21,7 @@ namespace Methods
         }
 
         /// <summary>Converts an integer number into its string name.</summary><param name="number">Original number as integer.</param><returns>The string value that matches.</returns>
-        static string NumberToDigit(int number)
+        public static string NumberToDigit(int number)
         {
             switch (number)
             {
@@ -39,16 +40,16 @@ namespace Methods
         }
 
         /// <summary>Finds the maximum element among an array of integers.</summary><param name="elements">Original array of integers.</param><returns>Maximum element found.</returns>
-        static int FindMax(params int[] elements)
+        public static int FindMax(params int[] elements)
         {
             if (elements == null || elements.Length == 0)
             {
-                throw new ArgumentOutOfRangeException("Invalid array!");
+                throw new ArgumentOutOfRangeException("Cannot find max in null or empty array!");
             }
 
             int max = elements[0];
 
-            for (int i = 1; i < elements.Length; i++)
+            for (int i = 0; i < elements.Length; i++)
             {
                 if (elements[i] > max)
                 {
@@ -59,34 +60,51 @@ namespace Methods
             return max;
         }
 
-        static void PrintAsNumber(object number, string format)
+        /// <summary>Prints a number to the standard console according to a predefined fixed format.</summary><param name="number">The number in object form.</param><param name="format">Formatting string for selecting among predefined fixed formats.</param>
+        public static void PrintAsNumber(object number, string format)
         {
-            if (format == "f")
+            string result;
+            switch (format)
             {
-                Console.WriteLine("{0:f2}", number);
+                case "f":
+                    {
+                        result = string.Format("{0:f2}", number);
+                        break;
+                    }
+
+                case "%":
+                    {
+                        result = string.Format("{0:p0}", number);
+                        break;
+                    }
+
+                case "r":
+                    {
+                        result = string.Format("{0,8}", number);
+                        break;
+                    }
+
+                default:
+                    {
+                        throw new ArgumentException("Invalid format string!");
+                    }
             }
-            if (format == "%")
-            {
-                Console.WriteLine("{0:p0}", number);
-            }
-            if (format == "r")
-            {
-                Console.WriteLine("{0,8}", number);
-            }
+
+            Console.WriteLine(result);
         }
 
-
-        static double CalcDistance(double x1, double y1, double x2, double y2,
-            out bool isHorizontal, out bool isVertical)
+        /// <summary>Calculates the distance between two points in two-dimensional space.</summary><param name="x1">Starting point's x-coordinate.</param><param name="y1">Starting point's y-coordinate.</param><param name="x2">Ending point's x-coordinate.</param><param name="y2">Ending point's y-coordinate.</param><param name="isHorizontal">A boolean parameter reflecting whether the line drawn is purely horizontal.</param><param name="isVertical">A boolean parameter reflecting whether the line drawn is purely vertical.</param><returns>The distance from point (x1, y1) to point (x2, y2) calculated using standard formula.</returns>
+        public static double CalcDistance(double x1, double y1, double x2, double y2, out bool isHorizontal, out bool isVertical)
         {
-            isHorizontal = (y1 == y2);
-            isVertical = (x1 == x2);
+            isHorizontal = y1 == y2;
+            isVertical = x1 == x2;
 
-            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            double distance = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
             return distance;
         }
 
-        static void Main()
+        /// <summary>Main exe starts here.</summary>
+        public static void Main()
         {
             Console.WriteLine(CalcTriangleArea(3, 4, 5));
 
@@ -104,13 +122,12 @@ namespace Methods
             Console.WriteLine("Vertical? " + vertical);
 
             Student peter = new Student() { FirstName = "Peter", LastName = "Ivanov" };
-            peter.OtherInfo = "From Sofia, born at 17.03.1992";
+            peter.OtherInfo = "From Sofia, born on 17.03.1992";
 
             Student stella = new Student() { FirstName = "Stella", LastName = "Markova" };
-            stella.OtherInfo = "From Vidin, gamer, high results, born at 03.11.1993";
+            stella.OtherInfo = "From Vidin, gamer, high results, born on 03.11.1993";
 
-            Console.WriteLine("{0} older than {1} -> {2}",
-                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+            Console.WriteLine("{0} older than {1} -> {2}", peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
         }
     }
 }
--- Module 2/High Quality Code I/homework_6_due_25.03.2017/Methods/Student.cs
+++ Module 2/High Quality Code I/homework_6_due_25.03.2017/Methods/Student.cs
@@ -1,20 +1,26 @@
-﻿using System;
-
+﻿//// <copyright file="Student.cs" company="indepentent developer">Copyright (c) Vassil Stoychev 2017. All rights reserved.</copyright>
 namespace Methods
 {
-    class Student
+    using System;
+
+    /// <summary>Represents a model student.</summary>
+    internal class Student
     {
+        /// <summary>Gets or sets the first name of a student.</summary>
         public string FirstName { get; set; }
+
+        /// <summary>Gets or sets the surname of a student.</summary>
         public string LastName { get; set; }
+
+        /// <summary>Gets or sets additional information pertaining to individual student.</summary>
         public string OtherInfo { get; set; }
 
+        /// <summary>Evaluates whether the method-caller instance of <see cref="Student"/> is older than another <see cref="Student"/> passed as parameter.</summary><param name="other">Student for comparison with <c>this</c>.</param><returns>True if <c>this</c> student is older than <c>other student</c>, false otherwise.</returns>
         public bool IsOlderThan(Student other)
         {
-            DateTime firstDate =
-                DateTime.Parse(this.OtherInfo.Substring(this.OtherInfo.Length - 10));
-            DateTime secondDate =
-                DateTime.Parse(other.OtherInfo.Substring(other.OtherInfo.Length - 10));
-            return firstDate > secondDate;
+            DateTime thisDateOfBirth = DateTime.Parse(this.OtherInfo.Substring(this.OtherInfo.Length - 10));
+            DateTime otherDateOfBirth = DateTime.Parse(other.OtherInfo.Substring(other.OtherInfo.Length - 10));
+            return thisDateOfBirth < otherDateOfBirth;
         }
     }
 }
