574ae72d5f34dca8c8c3aa06646b2ca592c5b4f7
stoychevvasko <stoychev.vassil@gmail.com>
2017-03-26 22:44:51 +0300

    JavaScript UI & DOM - Exam prep staging
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 2/JS III - UI & DOM/exam prep/exam 16.08.2016/2. File explorer/task/solution.js
@@ -0,0 +1,9 @@
+function solve() {
+    return function (fileContentsByName) {
+        // you solution
+    }
+}
+
+if (typeof module !== 'undefined') {
+    module.exports = solve;
+}
---
+++ Module 2/JS III - UI & DOM/exam prep/exam 16.08.2016/3. Forum/task/solution.js
@@ -0,0 +1,17 @@
+function solve() {
+	return function() {
+		var template = [
+			'first line',
+			'second line',
+			'...'
+		].join('\n');
+
+		return template;
+	}
+}
+
+// submit the above
+
+if(typeof module !== 'undefined') {
+	module.exports = solve;
+}
