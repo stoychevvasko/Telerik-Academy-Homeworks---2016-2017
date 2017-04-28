38ad62be03e9db3de8dfcbc0212703a36298a9c5
stoychevvasko <stoychev.vassil@gmail.com>
2017-04-28 17:56:48 +0300

    High Quality Code II - 03. Refactoring
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 2/High Quality Code II/03. Refactoring/MatrixRefactoring.Tests/CellTests.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="CellTests.cs" company="telerikacademy.com">for educational purposes only</copyright>
+namespace MatrixRefactoring.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    /// <summary>Custom tests.</summary>
+    [TestFixture]
+    public class CellTests
+    {
+        /// <summary>Test logic.</summary>
+        [Test]
+        public void Cell_WhenCreatedWithValidParameters_ShouldCreateNewCell()
+        {
+            Cell cell = new Cell(1, 1);
+        
+            var expectedX = 1;
+            var expectedY = 1;
+        
+            Assert.AreEqual(expectedX, cell.X);
+            Assert.AreEqual(expectedY, cell.Y);
+        }
+
+        /// <summary>Test logic.</summary>
+        [Test]
+        public void Cell_WhenSumTwoCellsWithInvaleidFirstCell_ShouldThrowArgumentNullException()
+        {
+            Cell secondCell = new Cell(1, 1);
+
+            Assert.Throws<ArgumentNullException>(() => new Cell((null + secondCell).X, (null + secondCell).Y));
+        }
+
+        /// <summary>Test logic.</summary>
+        [Test]
+        public void Cell_WhenSumTwoCellsWithInvaleidSecondCell_ShouldThrowArgumentNullException()
+        {
+            Cell firstCell = new Cell(1, 1);
+
+            Assert.Throws<ArgumentNullException>(() => new Cell((firstCell + null).X, (firstCell + null).Y));
+        }
+
+        /// <summary>Test logic.</summary>
+        public void Cell_WhenSumTwoCellsWithValeidCells_ShouldSumTheirXAndYCoordinates()
+        {
+            Cell firstCell = new Cell(1, 1);
+            Cell secondCell = new Cell(2, 2);
+            Cell result = firstCell + secondCell;
+
+            int expectedX = 3;
+            int expectedY = 3;
+
+            Assert.AreEqual(expectedX, result.X);
+            Assert.AreEqual(expectedY, result.Y);
+        }
+    }
+}
---
+++ Module 2/High Quality Code II/03. Refactoring/MatrixRefactoring.Tests/MatrixTests.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="MatrixTests.cs" company="telerikacademy.com">for educational purposes only</copyright>
+namespace MatrixRefactoring.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    /// <summary>Test logic.</summary>
+    [TestFixture]
+    public class MatrixTests
+    {
+        /// <summary>Test logic.</summary>
+        [Test]
+        public void Martix_WhenInvalidRows_ShouldThrowArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Matrix(-5, 2, 0));
+        }
+
+        /// <summary>Test logic.</summary>
+        [Test]
+        public void Martix_WhenInvalidCols_ShouldThrowArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Matrix(5, -2, 0));
+        }
+
+        /// <summary>Test logic.</summary>
+        [Test]
+        public void Martix_WhenInvalidStartIndexIsLess_ShouldThrowArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Matrix(5, 2, -2));
+        }
+
+        /// <summary>Test logic.</summary>
+        [Test]
+        public void Martix_WhenCallToStringMethod_ShouldReturnStringInExcactFormat()
+        {
+            Matrix matrix = new Matrix(2, 2, 0);
+            string matrixToString = matrix.ToString();
+
+            string expectedResult = "  0  0\r\n  0  0\r\n";
+
+            Assert.AreEqual(expectedResult, matrixToString);
+        }
+
+        /// <summary>Test logic.</summary>
+        [Test]
+        public void Martix_WhenFillMatrix_ShouldCalculateMatrixWithExcactValues()
+        {
+            Matrix matrix = new Matrix(2, 2, 0);
+            matrix.FillMatrix();
+            string matrixToString = matrix.ToString();
+
+            string expectedResult = "  1  4\r\n  3  2\r\n";
+
+            Assert.AreEqual(expectedResult, matrixToString);
+        }
+    }
+}
