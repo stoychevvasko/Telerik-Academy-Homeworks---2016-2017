6c9c1e84263b4fc143846794fb36992746598dcf
stoychevvasko <stoychev.vassil@gmail.com>
2017-05-03 18:01:05 +0300

    JavaScript IV - Applications - Workshop/Exam prep
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ Module 2/JS IV - Applications/04. Workshop - Self Manager/Solved/Workshop_25-April-2017_Self_Manager/public/js/app.js
@@ -0,0 +1,14 @@
+(function(){
+  var sammyApp = Sammy('#content', function(){
+    this.get('#/', function(){
+      this.redirect('#/home')
+    });
+
+    this.get('#/home', homeController.all);
+  });
+
+  $(function(){
+    sammyApp.run('#/');
+  })
+
+}());
